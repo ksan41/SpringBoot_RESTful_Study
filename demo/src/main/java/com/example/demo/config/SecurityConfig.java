515db89570de0c0aa72922b25ed17719e6a90464
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-07-23 00:33:37 +0900

    SecurityConfig/h2설정 코드

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -3,10 +3,25 @@ package com.example.demo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // /h2-console/뒤에 어떤 데이터 파일이 오더라도 통과되도록 함(permition처리)
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+
+        //쓰지 않을 부분은 사용하지 않도록 설정함.
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth)
         throws  Exception{
