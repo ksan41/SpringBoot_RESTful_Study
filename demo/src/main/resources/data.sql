2f64a038aef71553a32954822182177988e9496d
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-07-23 13:30:22 +0900

    data.sql 샘플데이터 추가

---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user value(1,sysdate(),'User1','test1','701010-1111111');
+insert into user value(2,sysdate(),'User1','test2','801010-2111111');
+insert into user value(3,sysdate(),'User1','test3','601010-1111111');
