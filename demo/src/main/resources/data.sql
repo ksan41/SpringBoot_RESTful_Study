e1255cdf736ae4b8ddaf601b2d19a20a68278bc2
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-07-24 01:50:19 +0900

    data.sql /h2 샘플데이터 추가
    오타 수정

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user value(1,sysdate(),'User1','test1','701010-1111111');
-insert into user value(2,sysdate(),'User1','test2','801010-2111111');
-insert into user value(3,sysdate(),'User1','test3','601010-1111111');
+insert into user values(1,sysdate(),'User1','test1','701010-1111111');
+insert into user values(2,sysdate(),'User1','test2','801010-2111111');
+insert into user values(3,sysdate(),'User1','test3','601010-1111111');
