a0b72b04a2f6210838e008e9aaf84c0af0256b67
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-07-24 15:39:54 +0900

    data.sql / post 샘플데이터 추가

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,3 +1,7 @@
 insert into user values(900001,sysdate(),'User1','test1','701010-1111111');
 insert into user values(900002,sysdate(),'User1','test2','801010-2111111');
 insert into user values(900003,sysdate(),'User1','test3','601010-1111111');
+
+
+insert into post values(10001, 'My first post', 900001);
+insert into post values(10002, 'My first post', 900001);
