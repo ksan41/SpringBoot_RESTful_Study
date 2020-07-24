fff046bd9a01c3727562d558b593c58447f76d03
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-07-24 15:39:37 +0900

    User.java/ posts 필드 생성.
    사용자가 작성한 게시글 정보를 저장하기 위한 필드.
    User 클래스의 샘플데이터에 영향이 가므로
    매개변수 생성자를 추가해 주었다.

--- demo/src/main/java/com/example/demo/user/User.java
+++ demo/src/main/java/com/example/demo/user/User.java
@@ -12,9 +12,11 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -53,4 +55,20 @@ public class User {
 
     @ApiModelProperty(notes="사용자 주민번호를 입력해 주세요.")
     private String ssn; //주민등록번호
+
+    // 사용자가 작성한 게시글 정보를 저장하기 위한 필드.
+    // @OneToMany(mappedBy = "user" )
+    // 이 필드는 일대 다의 관계를 가지며, user테이블과 매핑되고 있음을 의미.
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
+    // 게시글용 필드가 생성되었기 때문에
+    // 게시글 정보가 없이 사용자를 추가할 수 있도록 매개변수 생성자를 추가해준다.
+    public User(Integer id, @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.") String name, @Past Date joinDate, String password, String ssn) {
+        this.id = id;
+        this.name = name;
+        this.joinDate = joinDate;
+        this.password = password;
+        this.ssn = ssn;
+    }
 }
