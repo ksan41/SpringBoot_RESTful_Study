95642a313f1e8895086d1192d676a94e0e247d49
ksan41 <58001871+ksan41@users.noreply.github.com>
2020-07-24 14:58:19 +0900

    data.sql 샘플데이터 수정
    시퀀스 충돌 방지를 위해 임의로 pk 값을 변경.

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user values(1,sysdate(),'User1','test1','701010-1111111');
-insert into user values(2,sysdate(),'User1','test2','801010-2111111');
-insert into user values(3,sysdate(),'User1','test3','601010-1111111');
+insert into user values(900001,sysdate(),'User1','test1','701010-1111111');
+insert into user values(900002,sysdate(),'User1','test2','801010-2111111');
+insert into user values(900003,sysdate(),'User1','test3','601010-1111111');
